2e95269e294409b4e5f2e15eb2cbe4064401249b
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-11-30 20:30:40 +0530

    feat: basic lambda setup

---
+++ server/src/handlers/hello.ts
@@ -0,0 +1,9 @@
+export const hello = async (event: any) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from Lambda + TS!",
+      timestamp: new Date().toISOString(),
+    }),
+  };
+};
