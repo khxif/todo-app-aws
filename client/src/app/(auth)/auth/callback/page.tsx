560b01de0894414917f66608f18549a5d0ffb7d3
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-12-09 22:40:09 +0530

    feat: dockerized client

--- client/src/app/(auth)/auth/callback/page.tsx
+++ client/src/app/(auth)/auth/callback/page.tsx
@@ -1,20 +1,6 @@
-'use client';
-
 import { Card, CardContent } from '@/components/ui/card';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function AuthCallback() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const token = searchParams.get('token');
-  console.log(token);
-
-  useEffect(() => {
-    router.push('/');
-  },[])
-
   return (
     <Card>
       <CardContent>Loading....</CardContent>
