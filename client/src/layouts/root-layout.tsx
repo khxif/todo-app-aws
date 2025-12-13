61a92408d2fa270ae167fe81f41afa6f2c087d24
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-12-13 22:15:06 +0530

    feat: added spinner

--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Spinner } from '@/components/ui/spinner';
 import { useInitApp } from '@/hooks/use-init-app';
 import { useRootLayoutStore } from '@/stores/root-layout-store';
 
@@ -7,5 +8,11 @@ export function RootAppLayout({ children }: { children: React.ReactNode }) {
   const { isAppInitialized } = useRootLayoutStore();
   useInitApp();
 
-  return isAppInitialized ? children : <>Loaidng</>;
+  return isAppInitialized ? (
+    children
+  ) : (
+    <div className="w-full h-svh flex items-center justify-center">
+      <Spinner className="w-10 h-10 text-primary" />
+    </div>
+  );
 }
