2ea10d8148c925add8cc5e9a973dc87de8c95015
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-12-13 16:07:37 +0530

    feat: completed login page UI

--- client/src/app/(auth)/auth/login/page.tsx
+++ client/src/app/(auth)/auth/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Login() {
   async function loginWithGoogle() {
@@ -25,12 +25,16 @@ export default function Login() {
 
   return (
     <Card className="max-w-md w-full">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login Page</CardTitle>
-        <CardContent>
-          <Button onClick={loginWithGoogle}>Login with Google.</Button>
-        </CardContent>
+      <CardHeader className="text-center">
+        <CardTitle className="text-2xl">Welcome to Todo App</CardTitle>
+        <CardDescription className="text-muted">Please login to continue</CardDescription>
       </CardHeader>
+
+      <CardContent className="w-full">
+        <Button onClick={loginWithGoogle} className="w-full">
+          Login with Google.
+        </Button>
+      </CardContent>
     </Card>
   );
 }
